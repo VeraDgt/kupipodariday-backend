83c767f22dfe1b456de10c2eb160e25bf2621e92
VeraDgt <veradgt@gmail.com>
2024-07-14 17:02:24 +0300

    feat/wish entity

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,10 +1,14 @@
 import { Entity, Column, ManyToOne } from "typeorm";
 import { BaseEntityIdTimestamp } from "src/utils/entities/base.entity";
 import { User } from "src/users/entities/user.entity";
+import { Wish } from "src/wishes/entities/wish.entity";
 
 
 @Entity()
 export class Offer extends BaseEntityIdTimestamp {
   @ManyToOne(() => User, (user) => user.offers)
   user: User
+
+  @ManyToOne(() => Wish, (wish) => wish.offers)
+  item: Wish
 }
--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,10 +1,44 @@
-import { Entity, Column, ManyToOne } from "typeorm";
+import { Entity, Column, ManyToOne, OneToMany } from "typeorm";
 import { BaseEntityIdTimestamp } from "src/utils/entities/base.entity";
 import { User } from "src/users/entities/user.entity";
+import { IsString, Length, IsUrl, IsNumber, IsInt } from "class-validator";
+import { Offer } from "src/offers/entities/offer.entity";
 
 @Entity()
 export class Wish extends BaseEntityIdTimestamp {
+  @Column({ default: 'Пока ничего не рассказал о себе'})
+  @IsString()
+  @Length(1, 250)
+  name: string
+
+  @Column()
+  @IsUrl()
+  link: string
+
+  @Column()
+  @IsUrl()
+  image: string
+
+  @Column()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price: number
+
+  @Column()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  raised: number
 
   @ManyToOne(() => User, (user) => user.wishes)
   owner: User
+
+  @Column()
+  @IsString()
+  @Length(1, 1024)
+  description: string
+
+  @OneToMany(() => Offer, (offer) => offer.item)
+  offers: Offer[]
+
+  @Column()
+  @IsInt()
+  copied: number
 }
