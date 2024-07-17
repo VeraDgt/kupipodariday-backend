7582031af9be51bf23cadbe8e2a374aab239191c
VeraDgt <veradgt@gmail.com>
2024-07-17 20:09:34 +0300

    feat/guards

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post, UseGuards, Req, UseFilters, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signin(signInDto.username, /*signInDto.password*/);
+  }
+}
+
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
+  async signin(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
       access_token: this.jwtService.sign(payload),
