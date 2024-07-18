2046176fa12637536e219749c9bc2ad9d99e59b4
VeraDgt <veradgt@gmail.com>
2024-07-18 19:14:16 +0300

    feat/ user controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,28 @@
 import { Body, Controller, Post, UseGuards, Req, UseFilters, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from 'src/users/entities/user.entity';
+import { LocalGuard } from './guards/local.guard';
+import { UsersService } from 'src/users/users.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signin(signInDto.username, /*signInDto.password*/);
+  @UseGuards(LocalGuard)
+  @Post('signin')
+  async signIn(@Req() req: { user: Promise<User> }) {
+    return this.authService.signin(req.user);
+  }
+
+  @Post('signup')
+  async signUp(@Body() CreateUserDto: CreateUserDto) {
+    const user = await this.userService.create(CreateUserDto);
+    return await this.userService.create(user);
   }
 }
 
